0fde7391ecdf45608ff73301011d0da0b618c956
TwiceHere <temporaryt618@gmail.com>
2025-10-24 11:12:35 +0530

    nvim: Fix colorscheme error

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,7 +4,14 @@ return {
   name = "catppuccin", -- give it a name so Lazy can reference it
   opts = {
     flavour = "mocha", -- choose default flavor (latte, frappe, macchiato, mocha)
-    integrations = { ... }, -- integrations with other plugins (you listed them earlier)
+    integrations = {
+      bufferline = true,
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      telescope = true,
+      treesitter = true,
+    }, -- integrations with other plugins (you listed them earlier)
   },
   config = function(_, opts)
     require("catppuccin").setup(opts) -- apply your opts
