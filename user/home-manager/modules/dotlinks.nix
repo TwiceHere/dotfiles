e2635a544e6e23bed92b13c72900267b3843cc2a
TwiceHere <temporaryt618@gmail.com>
2026-06-20 10:36:38 +0530

    UPGRADE AND ADD fastfetch

--- user/home-manager/modules/dotlinks.nix
+++ user/home-manager/modules/dotlinks.nix
@@ -1,26 +1,38 @@
-{ config, pkgs, ... }: 
-
 {
-  home.file.".config/kitty".source = 
-    config.lib.file.mkOutOfStoreSymlink 
-    "/home/sonu/dotfiles/kitty"; 
+  config,
+  pkgs,
+  ...
+}: {
+  home.file.".config/kitty".source =
+    config.lib.file.mkOutOfStoreSymlink
+    "/home/sonu/dotfiles/kitty";
   xdg.configFile."hypr" = {
-    source = config.lib.file.mkOutOfStoreSymlink 
-    "/home/sonu/dotfiles/hypr"; 
-    recursive = true; 
-    force = true; 
+    source =
+      config.lib.file.mkOutOfStoreSymlink
+      "/home/sonu/dotfiles/hypr";
+    recursive = true;
+    force = true;
   };
   xdg.configFile."noctalia" = {
-    source = config.lib.file.mkOutOfStoreSymlink 
-      "/home/sonu/dotfiles/noctalia"; 
-    force = true; 
-  }; 
-  home.file.".local/bin/run".source = config.lib.file.mkOutOfStoreSymlink
-    "/home/sonu/dotfiles/myscripts/code_runner.sh"; 
+    source =
+      config.lib.file.mkOutOfStoreSymlink
+      "/home/sonu/dotfiles/noctalia";
+    force = true;
+  };
+  home.file.".local/bin/run".source =
+    config.lib.file.mkOutOfStoreSymlink
+    "/home/sonu/dotfiles/myscripts/code_runner.sh";
 
   xdg.configFile."tmux" = {
-    source = config.lib.file.mkOutOfStoreSymlink 
-    "/home/sonu/dotfiles/tmux"; 
-    force = true; 
-  }; 
+    source =
+      config.lib.file.mkOutOfStoreSymlink
+      "/home/sonu/dotfiles/tmux";
+    force = true;
+  };
+  xdg.configFile."fastfetch" = {
+    source =
+      config.lib.file.mkOutOfStoreSymlink
+      "/home/sonu/dotfiles/fastfetch";
+    force = true;
+  };
 }
