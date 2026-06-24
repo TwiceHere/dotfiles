fe91c32afd965d96bb12abe6869d5fc05b934759
TwiceHere <temporaryt618@gmail.com>
2026-06-24 08:29:34 +0530

    ADD keyd (remapped capslock to esc,ctrl)

--- system/modules/extra.nix
+++ system/modules/extra.nix
@@ -12,6 +12,39 @@
 
   nix.settings.trusted-users = ["root" "sonu"];
 
+  services.keyd = {
+    enable = true;
+
+    keyboards.default = {
+      ids = ["*"];
+      settings = {
+        main = {
+          capslock = "overload(control, esc)";
+        };
+      };
+    };
+  };
+
+  # services.kanata = {
+  #   enable = true;
+  #
+  #   keyboards.default = {
+  #     config = ''
+  #       (defsrc
+  #       caps
+  #       )
+  #
+  #       (defalias
+  #         escctrl (tap-hold 50 100 esc lctrl)
+  #       )
+  #
+  #       (deflayer base
+  #         @escctrl
+  #       )
+  #     '';
+  #   };
+  # };
+
   # programs.nix-ld = {
   #   enable = true;
   #   libraries = with pkgs; [
